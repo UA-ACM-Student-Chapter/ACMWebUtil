2458d1999579eeea282cca97752846bb024942b9
hunter <hunter@hajaka.com>
2018-03-05 20:48:44 -0600

    Added comment to IsPaidMessage

--- src/main/java/edu/ua/cs/acm/messages/IsPaidMessage.java
+++ src/main/java/edu/ua/cs/acm/messages/IsPaidMessage.java
@@ -3,6 +3,11 @@ package edu.ua.cs.acm.messages;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
+/**
+ * Created by Hunter Allen on 3/5/18
+ *
+ * Easier to serialize the JSON to a String with a message class
+ */
 public class IsPaidMessage {
     private String email;
 
