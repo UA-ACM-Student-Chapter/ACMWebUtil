9204f3a1617d9b5c00fe237d63f9e9c6cf3e7e52
David <damccoy1@crimson.ua.edu>
2018-07-07 23:28:19 -0500

    and

--- src/main/java/edu/ua/cs/acm/controllers/MemberController.java
+++ src/main/java/edu/ua/cs/acm/controllers/MemberController.java
@@ -130,7 +130,7 @@ public class MemberController {
                 emailService.sendMessage(new PaymentConfirmationEmailMessage(payingMember.getFirstName(), payingMember.getLastName(), payingMember.getCrimsonEmail(), message.getDatePaid(), "$10", paymentType, message.getPurchaseID()));
             }
 
-            return new ResponseEntity<>("{\"id\":\"" + message.getPurchaseID() + "\", \"email\":\"" + message.getEmail() + "\", \"name\":\"" + payingMember.getFirstName() + " " + payingMember.getLastName() + "\", \"date\": \"" + message.getDatePaid() + "\"}");
+            return new ResponseEntity<>("{\"id\":\"" + message.getPurchaseID() + "\", \"email\":\"" + message.getEmail() + "\", \"name\":\"" + payingMember.getFirstName() + " " + payingMember.getLastName() + "\", \"date\": \"" + message.getDatePaid() + "\"}", HttpStatus.OK);
         }
         else return ResponseEntity.ok("could not validate transaction");
     }
